fca3a082ebd04fb5a9fa0a80b25d623509f70998
Martin Alexanders Lopez Mamani <lopezalexanders@gmail.com>
2026-03-21 18:26:55 -0400

    Implementar CRUD de la tabla programs backend y frontend 2

--- packages/frontend/src/components/pages/ProgramasPage/EditProgramPage.tsx
+++ packages/frontend/src/components/pages/ProgramasPage/EditProgramPage.tsx
@@ -56,7 +56,7 @@ export function EditProgramPage() {
     }
   };
 
-  if (loading) return (
+  if (loadingData) return (
     <MainLayout>
       <div className="rounded-2xl border border-slate-200/80 bg-white p-8 shadow-sm ring-1 ring-slate-200/50 dark:border-slate-600 dark:bg-slate-800/95 dark:ring-slate-600/50 sm:p-10 w-full">
         <p className='"text-slate-600 dark:text-slate-300'>Cargando...</p>
